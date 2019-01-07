1fee423d398dc7a73f150a765582bd36c7d56530
JamilW <jamilweeks@yahoo.com>
2019-01-06 23:17:41 -0500

    made some changes to jquery code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,29 @@ var losses = 0;
 var genNumber;
 var crystalValue;
 var crystal;
-var crystalsArray = [
-    "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-mind-gem-min.jpg",
-    "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-reality-gem.jpg",
-    "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-space-stone.jpg",
-    "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-time-stone-t.jpg"
-];
+var crystal = {
+    blue:
+    {
+     name: "Blue",
+     value: 0
+    },
+    green:
+    {
+     name: "Green",
+     value: 0
+    },
+    red:
+    {
+     name: "Red",
+     value: 0
+    },
+    yellow:
+    {
+     name: "Yellow",
+     value: 0
+    }
+   };
 var totalScore = [];
-//var infinityStones = crystalsArray[Math.floor(Math.random() * crystalsArray.length)];
 
 // Random generated number for player
 function randomNumber() {
@@ -21,29 +36,62 @@ function randomNumber() {
 }
 randomNumber();
 
-// Random number generated for crystal images upon click
-$(document).ready(function() {
-    $(".img-fluid").on("click", function() {
-    score = "[i] + [i]";
-    for (var i = 0; i < 4; i++) {
-        randomNo = Math.floor(Math.random() * 12 - 1 + 1) + 1;
-        score = randomNo;
-        //$("#total-score").html(totalScore);
-        }
-        totalScore.push(score); //randomNo;
-$("#total-score").html(totalScore);
-    });
-    
-}); 
+// reset function
+function reset()    {
+    wins = 0;
+    losses = 0
+    totalScore = [];
+    randomNumber();
+}
 
 // Conditions for wins/losses
-    if(genNumber === totalScore)   {
-        wins++;
-        totalScore;
-    }
-    if(genNumber < totalScore)    {
-        losses++;
-        totalScore;
-    }
-   
+if(genNumber === totalScore)   {
+    wins += 1;
+    totalScore;
+    reset();
+}
+if(genNumber < totalScore)    {
+    losses += 1;
+    totalScore;
+    reset();
+}
 
+// Random number generated for crystal images upon click
+function getRandom()    {
+    $("#blue").on("click", function()   {
+    for(var i = 0; i < 1; i++) {
+        crystal.blue.value = Math.floor(Math.random() * 12 - 1 + 1) + 1; 
+            randomNo = crystal.blue.value;
+            $("#total-score").html(totalScore);
+            totalScore.push(randomNo);
+            }
+    $("#red").on("click", function()   {        
+    for(var i = 0; i < 1; i++) {        
+        crystal.red.value = Math.floor(Math.random() * 12 - 1 + 1) + 1; 
+            randomNo = crystal.red.value;
+            $("#total-score").html(totalScore);
+            totalScore.push(randomNo);
+            }
+    $("#green").on("click", function()   {        
+    for(var i = 0; i < 1; i++) {        
+        crystal.green.value = Math.floor(Math.random() * 12 - 1 + 1) + 1; 
+            randomNo = crystal.green.value;
+            $("#total-score").html(totalScore);
+            totalScore.push(randomNo);
+            }
+    $("#yellow").on("click", function()   {        
+    for(var i = 0; i < 1; i++) {        
+        crystal.yellow.value = Math.floor(Math.random() * 12 - 1 + 1) + 1; 
+            randomNo = crystal.yellow.value;
+            $("#total-score").html(totalScore);
+            totalScore.push(randomNo);
+                        }
+                    });    
+                 });        
+            });
+        });        
+    }
+    getRandom();
+    
+
+ 
