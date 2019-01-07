38c14324cc3e3aa831a2df30097bc94394b12183
JamilW <jamilweeks@yahoo.com>
2019-01-06 19:08:25 -0500

    figured out how to push value to empty total score array but needs tweaking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,18 @@
 var wins = 0;
 var losses = 0;
 var genNumber;
+var crystalValue;
+var crystal;
 var crystalsArray = [
     "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-mind-gem-min.jpg",
     "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-reality-gem.jpg",
     "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-space-stone.jpg",
     "assets/images/rsz_infinity-war-infinity-stone-infinity-gem-marvel-time-stone-t.jpg"
-]
-var totalScore;
+];
+var totalScore = [];
+//var infinityStones = crystalsArray[Math.floor(Math.random() * crystalsArray.length)];
 
-// Random generated number
+// Random generated number for player
 function randomNumber() {
     genNumber = (Math.floor(Math.random() * 120 - 19 + 1) + 19);
     console.log(genNumber);
@@ -18,5 +21,29 @@ function randomNumber() {
 }
 randomNumber();
 
-// Adding points to player's score with click on crystal image
+// Random number generated for crystal images upon click
+$(document).ready(function() {
+    $(".img-fluid").on("click", function() {
+    score = "[i] + [i]";
+    for (var i = 0; i < 4; i++) {
+        randomNo = Math.floor(Math.random() * 12 - 1 + 1) + 1;
+        score = randomNo;
+        //$("#total-score").html(totalScore);
+        }
+        totalScore.push(score); //randomNo;
+$("#total-score").html(totalScore);
+    });
+    
+}); 
+
+// Conditions for wins/losses
+    if(genNumber === totalScore)   {
+        wins++;
+        totalScore;
+    }
+    if(genNumber < totalScore)    {
+        losses++;
+        totalScore;
+    }
+   
 
